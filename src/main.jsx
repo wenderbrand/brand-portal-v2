687450aecd8e9aa20493ad8516f50581ac54872c
wenderbrand <contato.wendersouza@gmail.com>
2026-04-02 00:31:53 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/index.css'
-ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
